f70bcafc51f6440fe5a643e55e17dae80d4d8093
kaneko <bull.31415@gmail.com>
2020-08-24 11:42:45 +0900

    ログイン機能の作成

---
+++ mysite/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views 
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('signup/', views.SignUpView.as_view(), name = 'signup'),
+    path('<username>/', views.AccountDetailView.as_view(), name = 'userdetail')
+]
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,3 +126,5 @@ AUTH_USER_MODEL = 'accounts.User'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+
+LOGIN_REDIRECT_URL = '/'
