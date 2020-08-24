aac6e2c6822ff4853a69e8bd6f61992f0389c996
中川結衣 <nakagawayui@nakagawayuinoMacBook-Pro.local>
2020-08-24 17:55:24 +0900

    いいねとコメント追加

--- mysite/posts/models.py
+++ mysite/posts/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from accounts.models import User
+
 # Create your models here.
 
 class Post(models.Model):
     author = models.ForeignKey(
-        'accounts.User', on_delete = models.CASCADE
+        'accounts.User', on_delete = models.CASCADE,
     )
     title = models.CharField(
-        max_length = 200
+        max_length = 200,
     )
     text = models.TextField(
 
@@ -20,4 +21,26 @@ class Post(models.Model):
     created_at = models.DateTimeField(
         auto_now_add = True,
         blank = True,
+    )
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        'accounts.User', on_delete = models.CASCADE,
+    )
+
+    post = models.ForeignKey(
+        'posts.Post', on_delete = models.CASCADE,
+    )
+
+    text = models.TextField(
+        blank = True,
+    )
+
+class Like(models.Model):
+    author = models.ForeignKey(
+        'accounts.User', on_delete = models.CASCADE,
+    )
+
+    post = models.ForeignKey(
+        'posts.Post', on_delete = models.CASCADE,
     )
